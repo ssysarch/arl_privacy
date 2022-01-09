f3c915debb4480e507d4287edf85cd7c8e251237
Vishnu Vardhan Bachupally <vishnu1911@vpn-172-27-172-104.host.ucla.edu>
2022-01-08 21:30:01 -0800

    added run scripts

--- fairness-via-contrastive-estimation-master/src/shell/run_utkface.sh
+++ fairness-via-contrastive-estimation-master/src/shell/run_utkface.sh
@@ -1,3 +1,3 @@
 # ADV FORGETTING
-bash src/shell/utkface/run_adv_forgetting_health.sh
+bash src/shell/utkface/run_adv_forgetting_utkface.sh
 python3 -m src.scripts.eval_embeddings -D -f result/utkface/adv_forgetting -r result/eval/utkface -m "adv_forgetting" --force -c config/eval_config.py
